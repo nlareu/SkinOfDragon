d499f7371b854c738c9e404152ccc85044af8bcf
nlareu <nlareu2@gmail.com>
2018-08-31 00:03:35 -0300

    - Add "Demo - Adventure Genre".

---
+++ src/Demo - Adventure Genre/Assets/Scripts/TimeInterval.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TimeInterval
+{
+    public float Value { get; private set; }
+    public float Interval { get; private set; }
+
+    public TimeInterval(float interval)
+    {
+        this.Interval = interval;
+    }
+
+    public float Accumulate()
+    {
+        this.Value += Time.deltaTime;
+
+        return this.Value;
+    }
+}
